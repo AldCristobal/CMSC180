ae1fc3600827371ba82ef10006629dfedf49e9ed
AldCristobal <alcristobal1@up.edu.ph>
2026-02-14 16:08:38 +0800

    refactor program args and add threadcount

--- RA2/lab02.c
+++ RA2/lab02.c
@@ -13,7 +13,7 @@ int main (int argc, char *argv[]){
 
     //get matSize
     int matSize = 0;
-    sscanf(argv[1], "%i", &matSize);
+    sscanf(argv[3], "%i", &matSize);
     // printf("The size is %i\n",matSize);
     
     //build X and T matrices with matSize * matSize size
@@ -29,12 +29,16 @@ int main (int argc, char *argv[]){
     matPopulate(X, matSize, maxVal);
 
     int doPrint = 0;
-    sscanf(argv[3], "%i", &doPrint);
+    sscanf(argv[1], "%i", &doPrint);
 
     if (doPrint){
         printf("Initial Matrix: \n");
         matPrint(X, matSize);
     }
+
+    int threadCount = 0;
+    sscanf(argv[4], "%i", &threadCount);
+    printf("Thread count: %i\n", threadCount);
 
     //init and get starting time
     struct timespec time_before, time_after;
@@ -49,7 +53,7 @@ int main (int argc, char *argv[]){
         printf("Resulting Matrix: \n");
         matPrint(X, matSize);
     }
-    
+
     nanoseconds = (time_after.tv_sec - time_before.tv_sec) * 1000000000LL + (time_after.tv_nsec - time_before.tv_nsec);
     printf("Time taken: %lld nanoseconds\n", nanoseconds);
 
