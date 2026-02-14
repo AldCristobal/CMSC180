a9c385bc0acaa388b17918ca7f00d0b866f01000
AldCristobal <alcristobal1@up.edu.ph>
2026-02-14 16:26:10 +0800

    format array to matrix variable names

--- RA2/mmt.h
+++ RA2/mmt.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mmt(float** X, int arrSize, float* minXj, float* maxXj){
-    for (int i = 0; i < arrSize; i++) {       
-        for (int j = 0; j < arrSize; j++) {             
+void mmt(float** X, int matSize, float* minXj, float* maxXj){
+    for (int i = 0; i < matSize; i++) {       
+        for (int j = 0; j < matSize; j++) {             
             X[i][j] = (X[i][j] - minXj[j])/(maxXj[j]-minXj[j]);
             // printf("%f\n",X[i][j]);
         }
