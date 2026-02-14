481ab5ab0926f7b41f72d3b4e33b6d7da524832a
AldCristobal <alcristobal1@up.edu.ph>
2026-02-14 15:44:58 +0800

    test lab01 functionalities

--- RA2/lab02.c
+++ RA2/lab02.c
@@ -4,8 +4,33 @@
 #include <string.h>
 #include <pthread.h>
 #include "mmt.h"
+#include "arrayOps.h"
 
 int main (int argc, char *argv[]){
-    mmt(NULL, 0);
+
+    srand(time(NULL));  
+
+    //get arrSize
+    int arrSize = 0;
+    sscanf(argv[1], "%i", &arrSize);
+    printf("The size is %i\n",arrSize);
+    
+    //build X and T matrices with arrSize * arrSize size
+    float **X;
+    X = malloc(arrSize * sizeof(float *));
+    for (int i = 0; i < arrSize; i++){
+        X[i] = malloc(arrSize * sizeof(float));
+    }
+    printf("%i\n",arrSize);
+
+    arrPrint(X, arrSize);
+
+    int maxVal = 0;
+    sscanf(argv[2], "%i", &maxVal);
+    arrPopulate(X, arrSize, maxVal);
+
+    mmt(X, arrSize);
+
+    arrPrint(X, arrSize);
     return 0;
 }
