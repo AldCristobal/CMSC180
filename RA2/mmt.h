17f03340dc27120e54d4883161b09a94f5865940
AldCristobal <alcristobal1@up.edu.ph>
2026-02-24 20:27:16 +0800

    move minmax to mmt function

--- RA2/mmt.h
+++ RA2/mmt.h
@@ -18,16 +18,32 @@ void* mmt(void *arg) {
     int matSize = args->matSize;
     int startCol = args->startCol;
     int endCol = args->endCol;
-    float *minXj = args->minXj;
-    float *maxXj = args->maxXj;
 
-    for (int i = 0; i < matSize; i++) {
-        for (int j = startCol; j < endCol; j++) {
-            X[i][j] = (X[i][j] - minXj[j]) / (maxXj[j] - minXj[j]);
+    float minXj[matSize], maxXj[matSize];
+    for (int i = 0; i < matSize; i++){
+        minXj[i] = X[0][i];
+        maxXj[i] = X[0][i];
+    }
+    for (int i = 0; i < matSize; i++) {       
+        for (int j = 0; j < matSize; j++) {   
+            // printf("%f\n", X[i][j]);
+            if(minXj[j] > X[i][j]){
+                minXj[j] = X[i][j];
+            }
+            if(maxXj[j] < X[i][j]){
+                maxXj[j] = X[i][j];
+            }
         }
     }
 
-    //print submatrix after MMT
+    for (int i = 0; i < matSize; i++) {
+        int k = 0;
+        for (int j = startCol; j < endCol; j++) {
+            X[i][j] = (X[i][j] - minXj[k]) / (maxXj[k] - minXj[k]);
+            k++;
+        }
+    }
+
     if (args->doPrint) {
         printf("Thread handling columns %i to %i finished MMT. Submatrix:\n", startCol, endCol-1);
         for (int i = 0; i < matSize; i++) {
